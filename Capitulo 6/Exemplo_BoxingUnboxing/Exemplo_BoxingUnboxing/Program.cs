4599d908037418a936f0aff6a497bae1de3025a9
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-05-21 15:23:49 -0300

    "Exemplo de aplicação de boxing e unboxing"

---
+++ Capitulo 6/Exemplo_BoxingUnboxing/Exemplo_BoxingUnboxing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exemplo_BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo de boxing e Unboxing
+
+            //Boxing
+            //Tipo por referência recebendo tipo valor
+            int x = 20;
+            Object obj = x;
+
+            //Unboxing
+            //O processo inverso do Boxing
+            //Tipo valor recebendo tipo por referência
+            int y = (int) obj;
+
+        }
+    }
+}
