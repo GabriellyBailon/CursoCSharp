7346b38c373de1e2a7e9027c0d47d92f3db67661
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-05-24 09:26:36 -0300

    Exemplo Foreach adicionado.

---
+++ Capitulo 6/Exemplo_Foreach/Exemplo_Foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exemplo_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração na mesma linha
+            string[] vetorExemplo = new string[] {"Maria", "João", "Bob"};
+
+            Console.WriteLine("Exemplo Foreach");
+            foreach(string nome in vetorExemplo)
+            {
+                Console.WriteLine(nome);
+            }
+
+
+        }
+    }
+}
