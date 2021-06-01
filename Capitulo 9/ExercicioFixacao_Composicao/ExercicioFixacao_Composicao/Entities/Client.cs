43a9c6128658d4e5ff1bdb8287381066d60613fc
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-06-01 11:51:28 -0300

    Classe Client concluída.

--- Capitulo 9/ExercicioFixacao_Composicao/ExercicioFixacao_Composicao/Entities/Client.cs
+++ Capitulo 9/ExercicioFixacao_Composicao/ExercicioFixacao_Composicao/Entities/Client.cs
@@ -6,5 +6,22 @@ namespace ExercicioFixacao_Composicao.Entities
 {
     class Client
     {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+
+
+        //Construtores
+        public Client()
+        {
+
+        }
+
+        public Client(string nome, string email, DateTime dataDeNascimento)
+        {
+            Nome = nome;
+            Email = email;
+            DataDeNascimento = dataDeNascimento;
+        }
     }
 }
