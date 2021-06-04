e9637c8d92b0a1263b71d79555b26fb67890f1f2
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-06-04 15:18:43 -0300

    Exemplo prático usando os conceitos aprendidos até aqui

---
+++ Capitulo 10/Exercicio001_Heranca/Exercicio001_Heranca/Entities/Terceirizado.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio001_Heranca.Entities
+{
+    class Terceirizado : Employee
+    {
+        public double AditionalCharge { get; set; }
+
+        public Terceirizado()
+        {
+        }
+
+        public Terceirizado(string name, int hours, double valuePerHour, double aditionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AditionalCharge = aditionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + (1.1 * AditionalCharge);
+        }
+
+    }
+}
---
+++ Capitulo 10/Exercicio001_Heranca/Exercicio001_Heranca/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Exercicio001_Heranca.Entities;
+
+namespace Exercicio001_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTrabalhadores;
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Quantos trabalhadores serão cadastrados? ");
+            numTrabalhadores = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numTrabalhadores; i++)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine($"Dados do funcionário #{i + 1}:");
+                Console.Write("O trabalhador é terceirizado? ");
+                string resposta;
+                resposta = Console.ReadLine().ToUpper();
+
+                if(resposta[0] == 'S')
+                {
+                    string nome;
+                    int horas;
+                    double valorPorHora, despesaAdicional;
+                    
+                    Console.Write("Nome: ");
+                    nome = Console.ReadLine();
+                    Console.Write("Horas: ");
+                    horas = int.Parse(Console.ReadLine());
+                    Console.Write("Valor por hora: ");
+                    valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Despesa adicional: ");
+                    despesaAdicional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Employee terceirizado = new Terceirizado(nome, horas, valorPorHora, despesaAdicional);
+
+                    employees.Add(terceirizado);
+                }
+                else
+                {
+                    string nome;
+                    int horas;
+                    double valorPorHora;
+
+                    Console.Write("Nome: ");
+                    nome = Console.ReadLine();
+                    Console.Write("Horas: ");
+                    horas = int.Parse(Console.ReadLine());
+                    Console.Write("Valor por hora: ");
+                    valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Employee employee = new Employee(nome, horas, valorPorHora);
+
+                    employees.Add(employee);
+                }
+            }
+
+            Console.WriteLine("Pagamentos: ");
+
+            foreach (Employee trabalhador in employees)
+            {
+                Console.Write($"\n{trabalhador.Name} - $ {trabalhador.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+        }
+    }
+}
