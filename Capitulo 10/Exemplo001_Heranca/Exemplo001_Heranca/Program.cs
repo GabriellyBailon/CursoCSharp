91101f2f2fdd39574e375394637b69ee41368e02
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-06-04 09:10:54 -0300

    Criada pasta do Capítulo 10 e pdfs de referencia. Adicionado também o exemplo001 sobre Herança.

---
+++ Capitulo 10/Exemplo001_Heranca/Exemplo001_Heranca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Exemplo001_Heranca.Entities;
+
+namespace Exemplo001_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brawn", 100.00, 500);
+
+            //Vendo o saldo
+            Console.WriteLine(account.Balance);
+
+            //Não é possível alterar a propriedade diretamente nesta classe
+            //account.Balance = 200;
+        }
+    }
+}
