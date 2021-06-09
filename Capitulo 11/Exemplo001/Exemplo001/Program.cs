06df0d3e19a8b1d37658966fd987fd3f2893f34a
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-06-09 11:20:08 -0300

    Criação da pasta para conteúdo do Capítulo 11 e primeiro exemplo com try e catch sendo adicionados.

---
+++ Capitulo 11/Exemplo001/Exemplo001/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exemplo001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, resultado;
+
+            try
+            {
+                n1 = int.Parse(Console.ReadLine());
+                n2 = int.Parse(Console.ReadLine());
+
+                resultado = n1 / n2;
+
+                Console.WriteLine(resultado);
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Erro! Divisão por 0 não permitida!");
+            }
+
+            //Pode-se instanciar uma exceção do tipo para usar seus atributos e/ou métodos ou não
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro! O número digitado precisa ser um inteiro! {e.Message}");
+            }
+        }
+    }
+}
