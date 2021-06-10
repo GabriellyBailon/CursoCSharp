1c85af051db4dda85ceaf37e5f0ef2e139ea00ed
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-06-10 13:06:12 -0300

    Criada classe Cor.

---
+++ Capitulo 12/ProjetoJogoDeXadrez/console-xadrez/console-xadrez/tabuleiro/Cor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    public enum Cor
+    {
+        Branca,
+        Preta,
+
+        //Podem ser usadas em outros jogos
+        Amarela,
+        Azul,
+        Vermelha,
+        Laranja
+    }
+}
